1f4b69d2717dcc6874fd165beb00db6dc1e01976
asstronut <akbar.10119247@mahasiswa.unikom.ac.id>
2020-11-19 21:07:10 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello, world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati provident, facere et earum itaque delectus magnam ut rem libero? Cumque eligendi quibusdam saepe illum assumenda architecto sunt culpa blanditiis in.</p>
+    <ul>
+        <li>satu</li>
+        <li>emao</li>
+    </ul>
 </body>
 </html>
